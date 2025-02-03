c224019cc070b63897e7e130d59f6145fbb5e8fb
vesna <oksana.kaznina@emailmatrix.ru>
2025-02-03 11:27:00 +0500

    added solving tasks for 3 block

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let newArray = string.split('');
+  let newArray2 = [];
+  let count = 0;
+  if (size == undefined) {
+    return string;
+  } else { if (!(size == 0)) {
+    for (let i = 0; i < newArray.length; i++) {
+      if (newArray[i] == newArray[i + 1]) {
+        count += 1;
+        if (count < size) {
+          newArray2.push(newArray[i]);
+        }
+      } else {
+        count = 0;
+        newArray2.push(newArray[i]);
+      }
+    }
+  }
+  return newArray2.join('');
+  }
 }
+
+
+
+
