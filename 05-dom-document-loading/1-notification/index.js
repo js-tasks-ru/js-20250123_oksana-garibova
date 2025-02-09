b6e6bb5e356ac5b1dfaddaaf66e36dc8d3b7958d
vesna <oksana.kaznina@emailmatrix.ru>
2025-02-09 23:40:38 +0500

    add edit

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  element;
+  constructor(
+    message = '', {
+      duration = 1000,
+      type = '',
+    } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement(this.createElementTemplate());
+  }
 
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createClassElement() {
+    return `${this.type}`;
+  }
+
+  createElementTemplate() {
+    return `
+	 <div class="notification ${this.createClassElement()}" style="--value:20s">
+		<div class="timer"></div>
+		<div class="inner-wrapper">
+			<div class="notification-header">${this.type}</div>
+			<div class="notification-body">
+			  ${this.message}
+			</div>
+		</div>
+	</div>
+	 `;
+  }
+
+  show(tag) {
+    if (tag) {
+      tag.append(this.element);
+    } else {
+      document.body.append(this.element) 
+    }
+    setTimeout(()=> {this.remove()}, this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,67 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  element;
+  constructor(
+    headerConfig = [id = '', title = '', sortable = true, sortType = ''], 
+    data = [id = '', title = '', price = 0, sales = 0]
+  ) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+    this.element = this.createElement(this.createElementTemplate());
+  }
 
+  createElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createHeaderElementTemplate(header) {
+    return header.map(item => {
+      return `
+	  <div class="sortable-table__cell" data-id="${item['id']}" data-sortable="${item['sortable']}" data-order="${item['sortType']}">
+			<span>${item['title']}</span>
+			</div>
+	`;
+    }).join('');
+  }
+
+  createBodyElementTemplate(body) {
+    return body.map(item => {
+      return `
+			<a href="/products/3d-ochki-epson-elpgs03" class="sortable-table__row">
+			${this.headerConfig.map((item) => {
+				return `
+				<div class="sortable-table__cell">
+						${item['id']}
+					</div>
+				`;
+			}).join('')}	
+			</a>
+	`;
+    }).join('');
+  }
+
+  createElementTemplate() {
+    return `
+	 	<div class="sortable-table">
+
+			<div data-element="header" class="sortable-table__header sortable-table__row">
+				${this.createHeaderElementTemplate(this.headerConfig)}
+			</div>
+
+			<div data-element="body" class="sortable-table__body">
+				${this.createBodyElementTemplate(this.data)}
+			</div>
+		</div>
+	`;
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
   }
 }
 
